f434384ff1214fe3f55e99ac9620778b06e81d68
Rubilmax <rmilon@gmail.com>
2023-03-27 11:19:11 +0200

    Installed hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,72 @@
+import * as dotenv from "dotenv";
+import "hardhat-gas-reporter";
+import "hardhat-tracer";
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-vyper";
+
+dotenv.config();
+
+export const rpcUrl = process.env.RPC_HTTP_URL || process.env.RPC_URL || "https://rpc.ankr.com/eth";
+if (!rpcUrl) throw Error(`no RPC url provided`);
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1,
+      forking: {
+        url: rpcUrl,
+        blockNumber: 16_600_000,
+      },
+      gasPrice: 0,
+      initialBaseFeePerGas: 0,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+          viaIR: true,
+        },
+      },
+      {
+        version: "0.5.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  vyper: {
+    compilers: [
+      { version: "0.2.15" },
+      { version: "0.2.16" },
+      { version: "0.3.0" },
+      { version: "0.3.1" },
+      { version: "0.3.3" },
+    ],
+  },
+  gasReporter: {
+    currency: "EUR",
+  },
+  mocha: {
+    timeout: 300000,
+  },
+  tracer: {
+    defaultVerbosity: 1,
+    gasCost: true,
+  },
+};
+
+export default config;
